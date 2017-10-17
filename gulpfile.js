2a070cc8a54151425add9c799de9e2b848c53d7f
waynee95 <larsericmarquardt@mail.de>
2017-10-18 00:35:26 +0200

    UPDATE gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-"use strict";
+'use strict';
 
-const gulp = require("gulp");
-const babel = require("gulp-babel");
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const changed = require('gulp-changed');
 
-gulp.task("to-es5", () => gulp.src("./src/WAY_Core.js")
-    .pipe(babel())
-    .pipe(gulp.dest("./game/js/plugins/")));
+const paths = {
+    src: './src/*',
+    pluginFolder: './game/js/plugins/',
+    dist: './dist'
+};
+
+gulp.task('to-es5', () =>
+    gulp.src(paths.src)
+        .pipe(changed(paths.src))
+        .pipe(babel())
+        .pipe(gulp.dest(paths.pluginFolder)));
+
+gulp.task('build', () =>
+    gulp.src(paths.src)
+        .pipe(babel())
+        .pipe(gulp.dest(paths.dist)));
