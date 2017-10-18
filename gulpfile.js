b0ab4ec596df973b1e25d4dbe4a012f5b770b8a3
waynee95 <larsericmarquardt@mail.de>
2017-10-18 21:09:07 +0200

    UPDATE gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,36 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const changed = require('gulp-changed');
+const connect = require('gulp-connect');
+const open = require('gulp-open');
 
 const paths = {
-    src: './src/*',
-    pluginFolder: './game/js/plugins/',
+    game: './game',
+    index: './game/index.html',
+    src: './src/**/*.js',
+    plugins: './game/js/plugins/',
     dist: './dist'
 };
 
-gulp.task('to-es5', () =>
+gulp.task('to-es5', () => {
     gulp.src(paths.src)
         .pipe(changed(paths.src))
         .pipe(babel())
-        .pipe(gulp.dest(paths.pluginFolder)));
+        .pipe(gulp.dest(paths.plugins));
+});
 
-gulp.task('build', () =>
+gulp.task('build', () => {
     gulp.src(paths.src)
         .pipe(babel())
-        .pipe(gulp.dest(paths.dist)));
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('webserver', () => {
+    connect.server({
+        root: paths.game,
+        port: 8888,
+        livereload: true,
+        fallback: paths.index
+    });
+    gulp.src('').pipe(open({ app: 'firefox', uri: 'http://localhost:8888' }));
+});
