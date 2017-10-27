99fbeadcf220ed55ae5c409f5b16c24f53d310d4
waynee95 <larsericmarquardt@mail.de>
2017-10-27 03:32:22 +0200

    WAY_YEP_ItemCore v1.0.0

---
+++ dist/WAY_YEP_ItemCore.js
@@ -0,0 +1,149 @@
+/* globals WAY, WAYModuleLoader */
+// ===========================================================================
+// WAY_YEP_ItemCore.js
+// ===========================================================================
+/**
+ * @file Addon to Yanfly's Item Core Plugin.
+ * @author waynee95
+ * @version 1.0.0
+ */
+/*:
+@plugindesc Addon to Yanfly's Item Core Plugin. <WAY_YEP_ItemCore>
+@author waynee95
+
+@help
+=============================================================================
+ ■ Notetags
+==============================================================================
+-- Items, Weapons, Armors, Skills Notetag:
+<Icon Background: x>
+
+Specify an icon index that will show a certain icon behind the normal icon.
+
+==============================================================================
+ ■ Lunatic Mode - Custom Name Eval
+==============================================================================
+For those who would like to have different items to have the name based on
+certain conditions:
+
+Item, Weapon, Armor and Skill Notetag:
+<Custom Name Eval>
+name = "Holy Sword +" + v[42]
+</Custom Name Eval>
+
+==============================================================================
+ ■ Lunatic Mode - Custom TextColor Eval
+==============================================================================
+For those who would like to have different text colors of certain items based
+on an eval:
+
+Item, Weapon, Armor and Skill Notetag:
+<Custom TextColor Eval>
+var sum = item.params.reduce(function (a, b) {
+    return a + b;
+    }, 0);
+if (sum < 20) {
+    textColor = 14;
+} else {
+    textColor = 27;
+}
+</Custom TextColor Eval>
+
+==============================================================================
+ ■ Terms of Use
+==============================================================================
+Credit must be given to: waynee95
+Please don't share my plugins anywhere, except if you have my permissions.
+
+My plugins may be used in commercial and non-commercial products.
+*/
+
+'use strict';
+
+if (WAY === undefined) {
+    console.error('You need to install WAY_Core!'); //eslint-disable-line no-console
+    if (Utils.isNwjs() && Utils.isOptionValid('test')) {
+        var gui = require('nw.gui'); //eslint-disable-line
+        gui.Window.get().showDevTools();
+    }
+    SceneManager.stop();
+} else {
+    WAYModuleLoader.registerPlugin('WAY_YEP_ItemCore', '1.0.0', 'waynee95');
+}
+
+(function () {
+    var _WAY$Util = WAY.Util,
+        getNotetag = _WAY$Util.getNotetag,
+        getMultiLineNotetag = _WAY$Util.getMultiLineNotetag,
+        showError = _WAY$Util.showError,
+        trim = _WAY$Util.trim,
+        toInt = _WAY$Util.toInt;
+
+
+    var parseNotetags = function parseNotetags(obj) {
+        obj.customNameEval = getMultiLineNotetag(obj.note, 'Custom Name Eval', null, trim);
+        obj.customTextColorEval = getMultiLineNotetag(obj.note, 'custom textcolor eval', null, trim);
+        obj.iconBackground = getNotetag(obj.note, 'Icon Background', null, toInt);
+    };
+
+    WAY.EventEmitter.on('load-item-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-weapon-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-armor-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-skill-notetags', parseNotetags);
+
+    (function (Window_Base) {
+        var s = $gameSwitches._data;
+        var v = $gameVariables._data;
+        var p = $gameParty;
+
+        var evalCustomName = function evalCustomName(item) {
+            var customNameEval = item.customNameEval;
+
+            if (!customNameEval || customNameEval === '') return item.name;
+            var name = '';
+            try {
+                eval(customNameEval);
+            } catch (e) {
+                showError(e.message);
+            }
+            return name;
+        };
+
+        var evalCustomTextColor = function evalCustomTextColor(item) {
+            var customTextColorEval = item.customTextColorEval;
+
+            if (!customTextColorEval || customTextColorEval === '') return 0;
+            var textColor = 0;
+            try {
+                eval(customTextColorEval);
+            } catch (e) {
+                showError(e.message);
+            }
+            return textColor;
+        };
+
+        Window_Base.prototype.setItemTextColorEval = function (item) {
+            if (!item) return;
+            this._resetTextColor = evalCustomTextColor(item) || item.textColor;
+        };
+
+        Window_Base.prototype.drawItemName = function (item, x, y) {
+            var width = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 312;
+
+            if (item) {
+                this.setItemTextColor(item);
+                this.setItemTextColorEval(item);
+                var iconBoxWidth = Window_Base._iconWidth + 4;
+                if (item.iconBackground) {
+                    this.drawIcon(item.iconBackground, x + 2, y + 2);
+                }
+                this.drawIcon(item.iconIndex, x + 2, y + 2);
+                var itemName = evalCustomName(item);
+                this.resetTextColor();
+                this.drawText(itemName, x + iconBoxWidth, y, width - iconBoxWidth);
+                this._resetTextColor = undefined;
+                this.resetTextColor();
+            }
+        };
+    })(Window_Base);
+})(WAYModuleLoader.getModule('WAY_YEP_ItemCore'));
---
+++ src/WAY_YEP_ItemCore.js
@@ -0,0 +1,141 @@
+/* globals WAY, WAYModuleLoader */
+// ===========================================================================
+// WAY_YEP_ItemCore.js
+// ===========================================================================
+/**
+ * @file Addon to Yanfly's Item Core Plugin.
+ * @author waynee95
+ * @version 1.0.0
+ */
+/*:
+@plugindesc Addon to Yanfly's Item Core Plugin. <WAY_YEP_ItemCore>
+@author waynee95
+
+@help
+=============================================================================
+ ■ Notetags
+==============================================================================
+-- Items, Weapons, Armors, Skills Notetag:
+<Icon Background: x>
+
+Specify an icon index that will show a certain icon behind the normal icon.
+
+==============================================================================
+ ■ Lunatic Mode - Custom Name Eval
+==============================================================================
+For those who would like to have different items to have the name based on
+certain conditions:
+
+Item, Weapon, Armor and Skill Notetag:
+<Custom Name Eval>
+name = "Holy Sword +" + v[42]
+</Custom Name Eval>
+
+==============================================================================
+ ■ Lunatic Mode - Custom TextColor Eval
+==============================================================================
+For those who would like to have different text colors of certain items based
+on an eval:
+
+Item, Weapon, Armor and Skill Notetag:
+<Custom TextColor Eval>
+var sum = item.params.reduce(function (a, b) {
+    return a + b;
+    }, 0);
+if (sum < 20) {
+    textColor = 14;
+} else {
+    textColor = 27;
+}
+</Custom TextColor Eval>
+
+==============================================================================
+ ■ Terms of Use
+==============================================================================
+Credit must be given to: waynee95
+Please don't share my plugins anywhere, except if you have my permissions.
+
+My plugins may be used in commercial and non-commercial products.
+*/
+
+'use strict';
+
+if (WAY === undefined) {
+    console.error('You need to install WAY_Core!'); //eslint-disable-line no-console
+    if (Utils.isNwjs() && Utils.isOptionValid('test')) {
+        var gui = require('nw.gui'); //eslint-disable-line
+        gui.Window.get().showDevTools();
+    }
+    SceneManager.stop();
+} else {
+    WAYModuleLoader.registerPlugin('WAY_YEP_ItemCore', '1.0.0', 'waynee95');
+}
+
+(() => {
+    const {
+        getNotetag, getMultiLineNotetag, showError, trim, toInt
+    } = WAY.Util;
+
+    const parseNotetags = (obj) => {
+        obj.customNameEval = getMultiLineNotetag(obj.note, 'Custom Name Eval', null, trim);
+        obj.customTextColorEval = getMultiLineNotetag(obj.note, 'custom textcolor eval', null, trim);
+        obj.iconBackground = getNotetag(obj.note, 'Icon Background', null, toInt);
+    };
+
+    WAY.EventEmitter.on('load-item-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-weapon-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-armor-notetags', parseNotetags);
+    WAY.EventEmitter.on('load-skill-notetags', parseNotetags);
+
+    ((Window_Base) => {
+        const s = $gameSwitches._data;
+        const v = $gameVariables._data;
+        const p = $gameParty;
+
+        const evalCustomName = (item) => {
+            const { customNameEval } = item;
+            if (!customNameEval || customNameEval === '') return item.name;
+            const name = '';
+            try {
+                eval(customNameEval);
+            } catch (e) {
+                showError(e.message);
+            }
+            return name;
+        };
+
+        const evalCustomTextColor = (item) => {
+            const { customTextColorEval } = item;
+            if (!customTextColorEval || customTextColorEval === '') return 0;
+            const textColor = 0;
+            try {
+                eval(customTextColorEval);
+            } catch (e) {
+                showError(e.message);
+            }
+            return textColor;
+        };
+
+        Window_Base.prototype.setItemTextColorEval = function (item) {
+            if (!item) return;
+            this._resetTextColor = evalCustomTextColor(item) || item.textColor;
+        };
+
+        Window_Base.prototype.drawItemName = function (item, x, y, width = 312) {
+            if (item) {
+                this.setItemTextColor(item);
+                this.setItemTextColorEval(item);
+                const iconBoxWidth = Window_Base._iconWidth + 4;
+                if (item.iconBackground) {
+                    this.drawIcon(item.iconBackground, x + 2, y + 2);
+                }
+                this.drawIcon(item.iconIndex, x + 2, y + 2);
+                const itemName = evalCustomName(item);
+                this.resetTextColor();
+                this.drawText(itemName, x + iconBoxWidth, y, width - iconBoxWidth);
+                this._resetTextColor = undefined;
+                this.resetTextColor();
+            }
+        };
+    })(Window_Base);
+})(WAYModuleLoader.getModule('WAY_YEP_ItemCore'));
