ac787f7367c916fb6e856a8ffe18a0c45e5f3c4f
waynee95 <larsericmarquardt@mail.de>
2018-01-09 21:26:10 +0100

    fix

--- dist/WAY_Core.js
+++ dist/WAY_Core.js
@@ -3,7 +3,7 @@
 // WAY_Core.js
 // ===========================================================================
 /*:
-@plugindesc v1.9.3 WAY Core Utility Plugin. Place it above all WAY plugins. <WAY_Core>
+@plugindesc v1.9.4 WAY Core Utility Plugin. Place it above all WAY plugins. <WAY_Core>
 @author waynee95
 
 @help
@@ -216,7 +216,7 @@ var WAYModuleLoader = function () {
     };
 }();
 
-WAYModuleLoader.registerPlugin('WAY_Core', '1.9.3', 'waynee95');
+WAYModuleLoader.registerPlugin('WAY_Core', '1.9.4', 'waynee95');
 
 var WAYCore = window.WAYCore || {};
 var WAY = WAYCore;
