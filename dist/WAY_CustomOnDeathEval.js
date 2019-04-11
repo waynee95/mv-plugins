23ad63fb132bde68e0d5b1903fbd8ac508d48865
waynee95 <larsericmarquardt@mail.de>
2019-04-11 20:27:10 +0200

    WAY_CustomOnDeathEval v1.1.1
    Fix issue with YEP_X_ExtDot. It did not trigger the eval.

--- dist/WAY_CustomOnDeathEval.js
+++ dist/WAY_CustomOnDeathEval.js
@@ -4,7 +4,7 @@
 // ===========================================================================
 
 /*:
-@plugindesc v1.1.0 Run code when a battler dies. <WAY_CustomOnDeathEval>
+@plugindesc v1.1.1 Run code when a battler dies. <WAY_CustomOnDeathEval>
 
 @author waynee95
 
@@ -38,13 +38,13 @@ Forum Link: https://forums.rpgmakerweb.com/index.php?members/waynee95.88436/
 Website: http://waynee95.me/
 Discord Name: waynee95#4261
 */
-'use strict';
+"use strict";
 
-if (typeof WAY === 'undefined') {
-  console.error('You need to install WAY_Core!'); // eslint-disable-line no-console
+if (typeof WAY === "undefined") {
+  console.error("You need to install WAY_Core!"); // eslint-disable-line no-console
 
-  if (Utils.isNwjs() && Utils.isOptionValid('test')) {
-    var gui = require('nw.gui'); //eslint-disable-line
+  if (Utils.isNwjs() && Utils.isOptionValid("test")) {
+    var gui = require("nw.gui"); //eslint-disable-line
 
 
     gui.Window.get().showDevTools();
@@ -52,9 +52,9 @@ if (typeof WAY === 'undefined') {
 
   SceneManager.stop();
 } else {
-  WAYModuleLoader.registerPlugin('WAY_CustomOnDeathEval', '1.1.0', 'waynee95', {
-    name: 'WAY_Core',
-    version: '>= 2.0.0'
+  WAYModuleLoader.registerPlugin("WAY_CustomOnDeathEval", "1.1.1", "waynee95", {
+    name: "WAY_Core",
+    version: ">= 2.0.0"
   });
 }
 
@@ -64,22 +64,22 @@ if (typeof WAY === 'undefined') {
       trim = _WAY$Util.trim;
 
   var parseNotetags = function parseNotetags(obj) {
-    obj.customOnDeathEval = getMultiLineNotetag(obj.note, 'Custom On Death Eval', null, trim);
+    obj.customOnDeathEval = getMultiLineNotetag(obj.note, "Custom On Death Eval", null, trim);
   };
 
-  WAY.EventEmitter.on('load-actor-notetags', parseNotetags);
-  WAY.EventEmitter.on('load-class-notetags', parseNotetags);
-  WAY.EventEmitter.on('load-enemy-notetags', parseNotetags);
-  WAY.EventEmitter.on('load-armor-notetags', parseNotetags);
-  WAY.EventEmitter.on('load-weapon-notetags', parseNotetags);
-  WAY.EventEmitter.on('load-state-notetags', parseNotetags);
+  WAY.EventEmitter.on("load-actor-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-class-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-enemy-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-armor-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-weapon-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-state-notetags", parseNotetags);
 
   var byCustomDeathEval = function byCustomDeathEval(obj) {
-    return obj ? obj.customOnDeathEval : '';
+    return obj ? obj.customOnDeathEval : "";
   };
 
   var toCustomDeathEvalCode = function toCustomDeathEvalCode(acc, obj) {
-    return "".concat(acc, "\n").concat(obj.customOnDeathEval || '');
+    return "".concat(acc, "\n").concat(obj.customOnDeathEval || "");
   };
 
   var evalCustomOnDeathEval = function evalCustomOnDeathEval(code, user, killer) {
@@ -102,7 +102,7 @@ if (typeof WAY === 'undefined') {
 
 
   Game_Battler.prototype.customOnDeathEval = function () {
-    return this.states().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, '');
+    return this.states().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, "");
   }; //==========================================================================
   // Game_Actor
   //==========================================================================
@@ -110,9 +110,9 @@ if (typeof WAY === 'undefined') {
 
   Game_Actor.prototype.customOnDeathEval = function () {
     var code = Game_Battler.prototype.customOnDeathEval.call(this);
-    code += this.equips().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, '');
-    code += "\n".concat(this.currentClass().customOnDeathEval) || '';
-    code += "\n".concat(this.actor().customOnDeathEval) || '';
+    code += this.equips().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, "");
+    code += "\n".concat(this.currentClass().customOnDeathEval) || "";
+    code += "\n".concat(this.actor().customOnDeathEval) || "";
     return code;
   }; //==========================================================================
   // Game_Enemy
@@ -121,7 +121,7 @@ if (typeof WAY === 'undefined') {
 
   Game_Enemy.prototype.customOnDeathEval = function () {
     var code = Game_Battler.prototype.customOnDeathEval.call(this);
-    code += "\n".concat(this.enemy().customOnDeathEval) || '';
+    code += "\n".concat(this.enemy().customOnDeathEval) || "";
     return code;
   }; //==========================================================================
   // Game_Action
@@ -136,5 +136,75 @@ if (typeof WAY === 'undefined') {
     if (target.hp < 1 || target.isDead()) {
       evalCustomOnDeathEval(target.customOnDeathEval(), target, this.subject());
     }
-  };
-})(WAYModuleLoader.getModule('WAY_CustomOnDeathEval'));
+  }; // Compatibility with YEP_X_ExtDot
+
+
+  if (Imported.YEP_X_ExtDot) {
+    Game_Battler.prototype.processDamageOverTimeStateEffect = function (state) {
+      var stateId = state.id;
+      var state = $dataStates[stateId];
+      if (!state) return;
+      if (state.dotFormula === "") return;
+      var a = this.stateOrigin(stateId);
+      var b = this;
+      var user = this;
+      var target = this;
+      var origin = this.stateOrigin(stateId);
+      var s = $gameSwitches._data;
+      var v = $gameVariables._data;
+      var healing = false;
+      var variance = state.dotVariance;
+      var element = state.dotElement;
+      var code = state.dotFormula;
+
+      try {
+        eval(code);
+
+        if (healing) {
+          value = Math.abs(Math.max(0, value));
+        } else {
+          value = Math.abs(Math.max(0, value)) * -1;
+        }
+
+        value = this.applyDamageOverTimeVariance(value, variance);
+        value = this.applyDamageOverTimeElement(value, element);
+        value = Math.round(value);
+
+        if (value !== 0) {
+          this.clearResult();
+          this.gainHp(value);
+          this.startDamagePopup();
+
+          if (state.dotAnimation > 0) {
+            this.startAnimation(state.dotAnimation);
+          }
+
+          if (this.isDead()) {
+            ///--edit by waynee95
+            var evalCustomOnDeathEval = function evalCustomOnDeathEval(code, user, killer) {
+              var a = user;
+              var b = killer;
+              var s = $gameSwitches;
+              var v = $gameVariables;
+              var p = $gameParty;
+
+              try {
+                eval(code);
+              } catch (e) {
+                throw e;
+              }
+            };
+
+            evalCustomOnDeathEval(user.customOnDeathEval(), user, user); ///--edit by waynee95
+
+            this.performCollapse();
+          }
+
+          this.clearResult();
+        }
+      } catch (e) {
+        Yanfly.Util.displayError(e, code, "CUSTOM DOT " + stateId + " CODE ERROR");
+      }
+    };
+  }
+})(WAYModuleLoader.getModule("WAY_CustomOnDeathEval"));
--- src/WAY_CustomOnDeathEval.js
+++ src/WAY_CustomOnDeathEval.js
@@ -3,7 +3,7 @@
 // WAY_CustomOnDeathEval.js
 // ===========================================================================
 /*:
-@plugindesc v1.1.0 Run code when a battler dies. <WAY_CustomOnDeathEval>
+@plugindesc v1.1.1 Run code when a battler dies. <WAY_CustomOnDeathEval>
 
 @author waynee95
 
@@ -38,89 +38,169 @@ Website: http://waynee95.me/
 Discord Name: waynee95#4261
 */
 
-'use strict'
+"use strict";
 
-if (typeof WAY === 'undefined') {
-  console.error('You need to install WAY_Core!') // eslint-disable-line no-console
-  if (Utils.isNwjs() && Utils.isOptionValid('test')) {
-    var gui = require('nw.gui'); //eslint-disable-line
-    gui.Window.get().showDevTools()
+if (typeof WAY === "undefined") {
+  console.error("You need to install WAY_Core!"); // eslint-disable-line no-console
+  if (Utils.isNwjs() && Utils.isOptionValid("test")) {
+    var gui = require("nw.gui"); //eslint-disable-line
+    gui.Window.get().showDevTools();
   }
-  SceneManager.stop()
+  SceneManager.stop();
 } else {
-  WAYModuleLoader.registerPlugin('WAY_CustomOnDeathEval', '1.1.0', 'waynee95', {
-    name: 'WAY_Core',
-    version: '>= 2.0.0'
-  })
+  WAYModuleLoader.registerPlugin("WAY_CustomOnDeathEval", "1.1.1", "waynee95", {
+    name: "WAY_Core",
+    version: ">= 2.0.0"
+  });
 }
 
 ($ => {
-  const { getMultiLineNotetag, trim } = WAY.Util
+  const { getMultiLineNotetag, trim } = WAY.Util;
 
   const parseNotetags = obj => {
-    obj.customOnDeathEval = getMultiLineNotetag(obj.note, 'Custom On Death Eval', null, trim)
-  }
+    obj.customOnDeathEval = getMultiLineNotetag(
+      obj.note,
+      "Custom On Death Eval",
+      null,
+      trim
+    );
+  };
 
-  WAY.EventEmitter.on('load-actor-notetags', parseNotetags)
-  WAY.EventEmitter.on('load-class-notetags', parseNotetags)
-  WAY.EventEmitter.on('load-enemy-notetags', parseNotetags)
-  WAY.EventEmitter.on('load-armor-notetags', parseNotetags)
-  WAY.EventEmitter.on('load-weapon-notetags', parseNotetags)
-  WAY.EventEmitter.on('load-state-notetags', parseNotetags)
+  WAY.EventEmitter.on("load-actor-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-class-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-enemy-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-armor-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-weapon-notetags", parseNotetags);
+  WAY.EventEmitter.on("load-state-notetags", parseNotetags);
 
-  const byCustomDeathEval = obj => obj ? obj.customOnDeathEval : ''
-  const toCustomDeathEvalCode = (acc, obj) => `${acc}\n${obj.customOnDeathEval || ''}`
+  const byCustomDeathEval = obj => (obj ? obj.customOnDeathEval : "");
+  const toCustomDeathEvalCode = (acc, obj) =>
+    `${acc}\n${obj.customOnDeathEval || ""}`;
 
-  const evalCustomOnDeathEval = function (code, user, killer) {
+  const evalCustomOnDeathEval = function(code, user, killer) {
     /* eslint-disable */
-    const a = user
-    const b = killer
-    const s = $gameSwitches
-    const v = $gameVariables
-    const p = $gameParty
+    const a = user;
+    const b = killer;
+    const s = $gameSwitches;
+    const v = $gameVariables;
+    const p = $gameParty;
     try {
       eval(code);
       /* eslint-enable */
     } catch (e) {
-      throw e
+      throw e;
     }
-  }
+  };
 
   //==========================================================================
   // Game_Battler
   //==========================================================================
-  Game_Battler.prototype.customOnDeathEval = function () {
-    return this.states().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, '')
-  }
+  Game_Battler.prototype.customOnDeathEval = function() {
+    return this.states()
+      .filter(byCustomDeathEval)
+      .reduce(toCustomDeathEvalCode, "");
+  };
 
   //==========================================================================
   // Game_Actor
   //==========================================================================
-  Game_Actor.prototype.customOnDeathEval = function () {
-    let code = Game_Battler.prototype.customOnDeathEval.call(this)
-    code += this.equips().filter(byCustomDeathEval).reduce(toCustomDeathEvalCode, '')
-    code += `\n${this.currentClass().customOnDeathEval}` || ''
-    code += `\n${this.actor().customOnDeathEval}` || ''
-    return code
-  }
+  Game_Actor.prototype.customOnDeathEval = function() {
+    let code = Game_Battler.prototype.customOnDeathEval.call(this);
+    code += this.equips()
+      .filter(byCustomDeathEval)
+      .reduce(toCustomDeathEvalCode, "");
+    code += `\n${this.currentClass().customOnDeathEval}` || "";
+    code += `\n${this.actor().customOnDeathEval}` || "";
+    return code;
+  };
 
   //==========================================================================
   // Game_Enemy
   //==========================================================================
-  Game_Enemy.prototype.customOnDeathEval = function () {
-    let code = Game_Battler.prototype.customOnDeathEval.call(this)
-    code += `\n${this.enemy().customOnDeathEval}` || ''
-    return code
-  }
+  Game_Enemy.prototype.customOnDeathEval = function() {
+    let code = Game_Battler.prototype.customOnDeathEval.call(this);
+    code += `\n${this.enemy().customOnDeathEval}` || "";
+    return code;
+  };
 
   //==========================================================================
   // Game_Action
   //==========================================================================
-  $.alias.Game_Action_executeHpDamage = Game_Action.prototype.executeHpDamage
-  Game_Action.prototype.executeHpDamage = function (target, value) {
-    $.alias.Game_Action_executeHpDamage.call(this, target, value)
+  $.alias.Game_Action_executeHpDamage = Game_Action.prototype.executeHpDamage;
+  Game_Action.prototype.executeHpDamage = function(target, value) {
+    $.alias.Game_Action_executeHpDamage.call(this, target, value);
     if (target.hp < 1 || target.isDead()) {
-      evalCustomOnDeathEval(target.customOnDeathEval(), target, this.subject())
+      evalCustomOnDeathEval(target.customOnDeathEval(), target, this.subject());
     }
+  };
+
+  // Compatibility with YEP_X_ExtDot
+  if (Imported.YEP_X_ExtDot) {
+    Game_Battler.prototype.processDamageOverTimeStateEffect = function(state) {
+      var stateId = state.id;
+      var state = $dataStates[stateId];
+      if (!state) return;
+      if (state.dotFormula === "") return;
+      var a = this.stateOrigin(stateId);
+      var b = this;
+      var user = this;
+      var target = this;
+      var origin = this.stateOrigin(stateId);
+      var s = $gameSwitches._data;
+      var v = $gameVariables._data;
+      var healing = false;
+      var variance = state.dotVariance;
+      var element = state.dotElement;
+      var code = state.dotFormula;
+      try {
+        eval(code);
+        if (healing) {
+          value = Math.abs(Math.max(0, value));
+        } else {
+          value = Math.abs(Math.max(0, value)) * -1;
+        }
+        value = this.applyDamageOverTimeVariance(value, variance);
+        value = this.applyDamageOverTimeElement(value, element);
+        value = Math.round(value);
+        if (value !== 0) {
+          this.clearResult();
+          this.gainHp(value);
+          this.startDamagePopup();
+          if (state.dotAnimation > 0) {
+            this.startAnimation(state.dotAnimation);
+          }
+          if (this.isDead()) {
+            ///--edit by waynee95
+            var evalCustomOnDeathEval = function evalCustomOnDeathEval(
+              code,
+              user,
+              killer
+            ) {
+              var a = user;
+              var b = killer;
+              var s = $gameSwitches;
+              var v = $gameVariables;
+              var p = $gameParty;
+
+              try {
+                eval(code);
+              } catch (e) {
+                throw e;
+              }
+            };
+            evalCustomOnDeathEval(user.customOnDeathEval(), user, user);
+            ///--edit by waynee95
+            this.performCollapse();
+          }
+          this.clearResult();
+        }
+      } catch (e) {
+        Yanfly.Util.displayError(
+          e,
+          code,
+          "CUSTOM DOT " + stateId + " CODE ERROR"
+        );
+      }
+    };
   }
-})(WAYModuleLoader.getModule('WAY_CustomOnDeathEval'))
+})(WAYModuleLoader.getModule("WAY_CustomOnDeathEval"));
